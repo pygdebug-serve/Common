2b6bf9af1b3a4b7bf4da537f879615d182d77c24
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-07-16 23:18:33 +0900

    fix: Console features

--- src/main/java/tokyo/peya/lib/pygdebug/common/packets/debug/PacketServerConsoleOutput.java
+++ src/main/java/tokyo/peya/lib/pygdebug/common/packets/debug/PacketServerConsoleOutput.java
@@ -3,13 +3,14 @@ package tokyo.peya.lib.pygdebug.common.packets.debug;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import tokyo.peya.lib.pygdebug.common.Packet;
+import tokyo.peya.lib.pygdebug.common.PacketBase;
 
 import java.util.logging.LogRecord;
 
 @Value
 @AllArgsConstructor
 @Packet(value = 0x02, sender = Packet.Sender.SERVER)
-public class PacketServerConsoleOutput
+public class PacketServerConsoleOutput implements PacketBase
 {
     LogRecord context;
 }
