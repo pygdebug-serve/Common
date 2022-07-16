0943355eb1a3e51729f549807f09ee6eaaf55ab6
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-07-16 22:08:54 +0900

    fix: impl PacketBAse

--- src/main/java/tokyo/peya/lib/pygdebug/common/packets/main/PacketPygDebugAvailable.java
+++ src/main/java/tokyo/peya/lib/pygdebug/common/packets/main/PacketPygDebugAvailable.java
@@ -1,8 +1,9 @@
 package tokyo.peya.lib.pygdebug.common.packets.main;
 
 import tokyo.peya.lib.pygdebug.common.Packet;
+import tokyo.peya.lib.pygdebug.common.PacketBase;
 
 @Packet(value = 0x03, sender = Packet.Sender.SERVER)
-public class PacketPygDebugAvailable
+public class PacketPygDebugAvailable implements PacketBase
 {
 }
