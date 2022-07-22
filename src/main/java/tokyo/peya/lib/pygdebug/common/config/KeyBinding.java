6bbe1707a0d2b205492475b97dbc0a2103f9ae44
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-07-22 21:24:14 +0900

    refactor: remove array from keys

--- src/main/java/tokyo/peya/lib/pygdebug/common/config/KeyBinding.java
+++ src/main/java/tokyo/peya/lib/pygdebug/common/config/KeyBinding.java
@@ -1,21 +1,18 @@
 package tokyo.peya.lib.pygdebug.common.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class KeyBinding
 {
     private final String displayName;
-    private int[] keys;
+    private int key;
 
     public KeyBinding()
     {
         this("unknown", -1);
     }
 
-    public KeyBinding(String displayName, int... keys)
-    {
-        this.displayName = displayName;
-        this.keys = keys;
-    }
 }
